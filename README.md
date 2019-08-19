8b1ecf53b066627f2358c4b9c1ca2a69990eecb0
scocoyash <yashjain864@gmail.com>
2019-08-20 01:05:58 +0530

    adds more control flags and simple error handling

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Text-Editor-In-C
 A basic Text editor in C99 Language
+
+## Notes :
+
+### Step 1 - Conversion from Canonical Mode to Raw Mode
+***
+
+* Starting from the main function, we need to change the terminal mode to **'RAW'** mode. By default, the terminal is in **'CANONICAL'** mode i.e. it does not pass the input to the process unless you press the Enter key.
+
+* We will need the *termios* library to change the terminal attributes so importing that firstly.
+
+* Now we **disable** various attributes of the terminal using flags : 
+
+   * *ECHO* [termios.h] - The ECHO attribute causes each key you type to be printed to the terminal, so you can see what you’re typing. We do not need such feature for the text-editor
+
+   * *ICANON* [termios.h] - Turns off Canonical mode; enables us to read the input byte-by-byte rather than line by line
+
+   * *ISIG* [termios.h] - Disable **Ctrl+Z** (26-byte SIGTSTP signal to the current process) and **Ctrl+C** (3-byte SIGINT signal to the current process) signals from user input
+
+   * *IXON* [termios.h] - Turns off **Ctrl+S** and **Ctrl+Q** software control signals
+
+   * *IEXTEN* [termios.h] - Turns off **Ctrl+V** and **Ctrl+O** software control signals
+
+   * *ICRNL* [termios.h] - Turns off **Ctrl+M** and **ENTER** transformation to Carriage Returns by the terminal.
+
+* Also disable many extra signals from wrong inputs. 
+
+* Do not forget to disable the RAW mode at program exit; using **atexit()** from [stdlib.h] library.
+
+* It’s time to clean up the code by adding some *error handling*:
+
+    * perror() [stdio.h] - looks at the global errno variable and prints a descriptive error message
+
+    * exit() [stdlib.h] - exit the program with an exit status of 1
+
+
--- stex.c
+++ stex.c
@@ -1,8 +1,12 @@
+/*** includes ***/
 #include<ctype.h>
 #include<stdio.h>
+#include <errno.h>
 #include<termios.h>
 #include<unistd.h>
 #include<stdlib.h>
+
+/*** data structures ***/
 
 /** @param 
 	original_terminos
@@ -10,37 +14,56 @@
 */
 struct termios original_termios;
 
+/*** terminal ***/
+
+/* error logging function */
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void exitRawMode(){
-	// leave the terminal attributes as they were when exiting
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+	/* leave the terminal attributes as they were when exiting */
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1) die("tcsetattr");
 }
 
 void enterRawMode(){
-	tcgetattr(STDIN_FILENO, &original_termios);
-	// whenever exiting the program, restore terminal attribute states
+	if (tcgetattr(STDIN_FILENO, &original_termios) == -1) die("tcgetattr");
+	/* whenever exiting the program, restore terminal attribute states */
 	atexit(exitRawMode);
 
 	struct termios raw = original_termios;
 	// flag - IXON turns ctrl+s && ctrl+q software signals off
 	// flag - ICRNL turns ctrl+m carriage return off
-	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	// flag - OPOST turns post-processing of output off
 	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	// flag - ICANON turns canonical mode off
 	// flag - ISIG turns ctrl+c && ctrl+z signals off
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+	/* adding timeouts for read */
+	raw.c_cc[VMIN] = 0;
+  	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main(){
 	enterRawMode();
-	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1 && c!='q'){
-	 if (iscntrl(c)) {
+	
+	while (1) {
+    	char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");		
+		if (iscntrl(c)) {
      	  printf("%d\n", c);
-    	 } else {
+    	} else {
       	  printf("%d ('%c')\r\n", c, c);
-    	 }
+    	}
+		if (c == 'q') break;
 	}
 	return 0;
 }
